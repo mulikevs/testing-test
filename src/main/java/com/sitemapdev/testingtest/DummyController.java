03372f5ff91e5454b64f9d8d04124e5e66cb913c
Kelvin M. Mwau [ICT] <mulikevs@gmail.com>
2024-04-02 11:44:38 +0300

    updated package name

--- src/main/java/com/sitemapdev/testingtest/DummyController.java
+++ src/main/java/com/sitemapdev/testingtest/DummyController.java
@@ -1,4 +1,4 @@
-package com.coopbank.testingtest;
+package com.sitemapdev.testingtest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
